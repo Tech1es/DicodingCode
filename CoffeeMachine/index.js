742a59c4183b9800d0b1013e0b79a1b6b31668c8
Tech1es <iqbalfaiz530@gmail.com>
2023-09-14 16:32:25 +0700

    Dicoding Submission

---
+++ CoffeeMachine/index.js
@@ -0,0 +1,16 @@
+// import { coffeeStock as stock, isCoffeeMachineReady } from "./state.js";
+
+// console.log(stock);
+// console.log(isCoffeeMachineReady);
+
+const json = '{ "name": "Yoda", "age": 20 }';
+
+try {
+  const user = JSON.parse(json);
+
+  console.log(user.name);
+  console.log(user.age);
+} catch (error) {
+  console.log(error.name);
+  console.log(error.message);
+}
